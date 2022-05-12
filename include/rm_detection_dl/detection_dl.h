1a50d7e27e76d900e0c6b5f4fe29fad4e012c62b
LHTcode <LHT13535837097@163.com>
2022-05-13 00:19:14 +0800

    Meet a problem:Can not load detction_dl nodelet.

--- include/rm_detection_dl/detection_dl.h
+++ include/rm_detection_dl/detection_dl.h
@@ -4,10 +4,12 @@
 #pragma once
 #include <nodelet/nodelet.h>
 #include <ros/ros.h>
+#include <vector>
+#include <opencv4/opencv2/opencv.hpp>
 #include <pluginlib/class_list_macros.h>
 #include <inference_engine.hpp>
-#include <vector>
-
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 
 namespace rm_detection_dl {
 class DetectionDL : public nodelet::Nodelet
@@ -16,8 +18,12 @@ public:
     DetectionDL() = default;
     void initialize();
 
-
 private:
     virtual void onInit();
+    void receiverCallback(const sensor_msgs::ImageConstPtr& img_msg, const sensor_msgs::CameraInfoConstPtr& img_info_msg);
+    std::shared_ptr<image_transport::ImageTransport> it_;
+    image_transport::CameraSubscriber img_sub_;
+    cv::Mat origin_img_;
+
 };
 }
--- src/detection_dl.cpp
+++ src/detection_dl.cpp
@@ -1,17 +1,32 @@
 //
 // Created by lihanting on 2022/5/12.
 //
-
 #include <rm_detection_dl/detection_dl.h>
 
 namespace rm_detection_dl{
 void DetectionDL::onInit()
 {
     NODELET_DEBUG("Initializing nodelet...");
-}
-void DetectionDL::initialize() {
+    auto& nh = getPrivateNodeHandle();
+    this->it_ = std::make_shared<image_transport::ImageTransport>(nh);
+    img_sub_ = it_->subscribeCamera(
+            "/galaxy_camera/image_raw" ,
+            10,
+            &DetectionDL::receiverCallback, this,
+            image_transport::TransportHints("raw",ros::TransportHints()));
 
 }
+void DetectionDL::initialize()
+{
+
+}
+void DetectionDL::receiverCallback(const sensor_msgs::ImageConstPtr& img_msg, const sensor_msgs::CameraInfoConstPtr& img_info_msg)
+{
+    cv_bridge::CvImageConstPtr cv_cptr = cv_bridge::toCvCopy(img_msg);
+//    cv_bridge::CvImageConstPtr cv_cptr = cv_bridge::toCvShare(img_msg);
+    cv_cptr->image.copyTo(origin_img_);
+}
+
 
 PLUGINLIB_EXPORT_CLASS(rm_detection_dl::DetectionDL, nodelet::Nodelet)
 }
